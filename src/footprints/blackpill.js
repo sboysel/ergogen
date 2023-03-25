74e031cd8d3a9016c9676ce3ffb0d9f7d1cebf58
Sam Boysel <sboysel@gmail.com>
2023-03-24 20:50:28 -0400

    blackpill footprint: nest nets as params

--- src/footprints/blackpill.js
+++ src/footprints/blackpill.js
@@ -1,50 +1,50 @@
 // https://raw.githubusercontent.com/SecretPocketCat/ergogen/main/src/footprints/blackpill_f4x1.js
 
 module.exports = {
-  nets: {
-    GND: "GND",
-    P5V1: "P5V1",
-    VDD24: "VDD24",
-    B10: "B10",
-    B2: "B2",
-    B1: "B1",
-    B0: "B0",
-    A7: "A7",
-    A6: "A6",
-    A5: "A5",
-    A4: "A4",
-    A3: "A3",
-    A2: "A2",
-    A1: "A1",
-    A0: "A0",
-    R: "R",
-    C15: "C15",
-    C14: "C14",
-    C13: "C13",
-    VB: "VB",
-    B12: "B12",
-    B13: "B13",
-    B14: "B14",
-    B15: "B15",
-    A8: "A8",
-    A9: "A9",
-    A10: "A10",
-    A11: "A11",
-    A12: "A12",
-    A15: "A15",
-    B3: "B3",
-    B4: "B4",
-    B5: "B5",
-    B6: "B6",
-    B7: "B7",
-    B8: "B8",
-    B9: "B9",
-    P5V2: "P5V2",
-    VDD48: "VDD48",
-  },
   params: {
     class: "MCU",
     orientation: "up",
+    nets: {
+      GND: "GND",
+      P5V1: "P5V1",
+      VDD24: "VDD24",
+      B10: "B10",
+      B2: "B2",
+      B1: "B1",
+      B0: "B0",
+      A7: "A7",
+      A6: "A6",
+      A5: "A5",
+      A4: "A4",
+      A3: "A3",
+      A2: "A2",
+      A1: "A1",
+      A0: "A0",
+      R: "R",
+      C15: "C15",
+      C14: "C14",
+      C13: "C13",
+      VB: "VB",
+      B12: "B12",
+      B13: "B13",
+      B14: "B14",
+      B15: "B15",
+      A8: "A8",
+      A9: "A9",
+      A10: "A10",
+      A11: "A11",
+      A12: "A12",
+      A15: "A15",
+      B3: "B3",
+      B4: "B4",
+      B5: "B5",
+      B6: "B6",
+      B7: "B7",
+      B8: "B8",
+      B9: "B9",
+      P5V2: "P5V2",
+      VDD48: "VDD48",
+    },
   },
   body: (p) => {
     function standard(silk_layer) {
@@ -83,86 +83,86 @@ module.exports = {
 
         ${"" /* actual pins */}
         (pad 40 thru_hole circle (at ${def_pos}7.62 0 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.P5V1.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.P5V1.str})
         (pad 39 thru_hole circle (at ${def_pos}7.62 2.54 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.GND.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.GND.str})
         (pad 38 thru_hole circle (at ${def_pos}7.62 5.08 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.VDD24.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.VDD24.str})
         (pad 37 thru_hole circle (at ${def_pos}7.62 7.62 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B10.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B10.str})
         (pad 36 thru_hole circle (at ${def_pos}7.62 10.16 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B2.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B2.str})
         (pad 35 thru_hole circle (at ${def_pos}7.62 12.7 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B1.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B1.str})
         (pad 34 thru_hole circle (at ${def_pos}7.62 15.24 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B0.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B0.str})
         (pad 33 thru_hole circle (at ${def_pos}7.62 17.78 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A7.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A7.str})
         (pad 32 thru_hole circle (at ${def_pos}7.62 20.32 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A6.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A6.str})
         (pad 31 thru_hole circle (at ${def_pos}7.62 22.86 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A5.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A5.str})
         (pad 30 thru_hole circle (at ${def_pos}7.62 25.4 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A4.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A4.str})
         (pad 29 thru_hole circle (at ${def_pos}7.62 27.94 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A3.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A3.str})
         (pad 28 thru_hole circle (at ${def_pos}7.62 30.48 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A2.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A2.str})
         (pad 27 thru_hole circle (at ${def_pos}7.62 33.02 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A1.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A1.str})
         (pad 26 thru_hole circle (at ${def_pos}7.62 35.56 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A0.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A0.str})
         (pad 25 thru_hole circle (at ${def_pos}7.62 38.1 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.R.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.R.str})
         (pad 24 thru_hole circle (at ${def_pos}7.62 40.64 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.C15.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.C15.str})
         (pad 23 thru_hole circle (at ${def_pos}7.62 43.18 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.C14.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.C14.str})
         (pad 22 thru_hole circle (at ${def_pos}7.62 45.72 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.C13.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.C13.str})
         (pad 21 thru_hole circle (at ${def_pos}7.62 48.26 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.VB.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.VB.str})
   
         (pad 1 thru_hole rect (at ${def_neg}7.62 0 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B12.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B12.str})
         (pad 2 thru_hole circle (at ${def_neg}7.62 2.54 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B13.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B13.str})
         (pad 3 thru_hole circle (at ${def_neg}7.62 5.08 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B14.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B14.str})
         (pad 4 thru_hole circle (at ${def_neg}7.62 7.62 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B15.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B15.str})
         (pad 5 thru_hole circle (at ${def_neg}7.62 10.16 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A8.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A8.str})
         (pad 6 thru_hole circle (at ${def_neg}7.62 12.7 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A9.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A9.str})
         (pad 7 thru_hole circle (at ${def_neg}7.62 15.24 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A10.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A10.str})
         (pad 8 thru_hole circle (at ${def_neg}7.62 17.78 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A11.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A11.str})
         (pad 9 thru_hole circle (at ${def_neg}7.62 20.32 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A12.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A12.str})
         (pad 10 thru_hole circle (at ${def_neg}7.62 22.86 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.A15.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.A15.str})
         (pad 11 thru_hole circle (at ${def_neg}7.62 25.4 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B3.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B3.str})
         (pad 12 thru_hole circle (at ${def_neg}7.62 27.94 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B4.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B4.str})
         (pad 13 thru_hole circle (at ${def_neg}7.62 30.48 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B5.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B5.str})
         (pad 14 thru_hole circle (at ${def_neg}7.62 33.02 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B6.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B6.str})
         (pad 15 thru_hole circle (at ${def_neg}7.62 35.56 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B7.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B7.str})
         (pad 16 thru_hole circle (at ${def_neg}7.62 38.1 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B8.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B8.str})
         (pad 17 thru_hole circle (at ${def_neg}7.62 40.64 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.B9.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.B9.str})
         (pad 18 thru_hole circle (at ${def_neg}7.62 43.18 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.P5V2.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.P5V2.str})
         (pad 19 thru_hole circle (at ${def_neg}7.62 45.72 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.GND.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.GND.str})
         (pad 20 thru_hole circle (at ${def_neg}7.62 48.26 ${p.rot
-        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.net.VDD48.str})
+        }) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) ${p.nets.VDD48.str})
       `;
     }
 
