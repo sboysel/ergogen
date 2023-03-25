71332d16eec889826b77b5a2dd1d5cc0030a1f88
Sam Boysel <sboysel@gmail.com>
2023-03-25 18:03:42 -0400

    m2 footprint: fix param names

--- src/footprints/m2.js
+++ src/footprints/m2.js
@@ -6,7 +6,7 @@ module.exports = {
   body: p => {
     return `(module DrillHole (layer "F.Cu") (tedit 5F880A3E)
     ${p.at}
-  (pad "" thru_hole circle (at 0 0) (size ${p.param.pad} ${p.param.pad}) (drill ${p.param.drill}) (layers *.Cu *.Mask) (tstamp 41c9d83b-325c-4159-a170-61d8654be338))
+  (pad "" thru_hole circle (at 0 0) (size ${p.pad} ${p.pad}) (drill ${p.drill}) (layers *.Cu *.Mask) (tstamp 41c9d83b-325c-4159-a170-61d8654be338))
 )
 `
   }
