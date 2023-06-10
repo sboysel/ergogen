40c1c5a9b45e6f27438c1ac03deb7bdfce9f0c75
Sam Boysel <sboysel@gmail.com>
2023-06-10 12:12:19 -0400

    adding diode footprint from MvEerd/ergogen

--- src/footprints/diode.js
+++ src/footprints/diode.js
@@ -1,14 +1,18 @@
+// src: MvEerd/ergogen
 module.exports = {
-    params: {
-        designator: 'D',
-	smd: true,
-	tht: true,
+    nets: {
         from: undefined,
         to: undefined
     },
-    body: p => {
-	const standard = `
+    params: {
+        class: 'D',
+        through_hole: true,
+        via_in_pad: false
+    },
+    body: p => `
+  
     (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
+
 
         ${p.at /* parametric position */}
 
@@ -31,38 +35,33 @@ module.exports = {
         (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
         (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
         (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
+
+        ${ p.param.via_in_pad ?
+            `
+             ${''/* Vias in SMD pads */}   
+             (pad 1 thru_hole rect (at -1.65 0 ${ p.rot }) (size 0.9 1.2) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${ p.net.to.str })
+             (pad 2 thru_hole rect (at 1.65 0 ${ p.rot }) (size 0.9 1.2) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${ p.net.from.str })
+            `
+        :
+            `
+             ${ ''/* SMD pads on both sides */ }
+             (pad 1 smd rect (at -1.65 0 ${ p.rot }) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${ p.net.to.str })
+             (pad 2 smd rect (at 1.65 0 ${ p.rot }) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${ p.net.from.str })
+             (pad 1 smd rect (at -1.65 0 ${ p.rot }) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${ p.net.to.str })
+             (pad 2 smd rect (at 1.65 0 ${ p.rot }) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${ p.net.from.str })
+            `
+        }
+        
+        ${ p.param.through_hole === false ?
+            ''
+        :
+            `
+             ${''/* THT terminals */}
+             (pad 1 thru_hole circle (at 3.81 0 ${ p.rot }) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${ p.net.from.str })
+             (pad 2 thru_hole rect (at -3.81 0 ${ p.rot }) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${ p.net.to.str })
+            `
+        }
+
+    )
     `
-    const smd = `
-        ${''/* SMD pads on both sides */}
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.from.str})
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.from.str})
-    `
-    const tht = `
-	${''/* THT terminals */}
-        (pad 1 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.to.str})
-        (pad 2 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.from.str})
-    `
-    if(p.smd & p.tht) {
-	return `
-	${standard}
-	${smd}
-	${tht}
-	)
-	`
-    } else if(p.smd) {  
-	return `
-	${standard}
-	${smd}
-	)
-	`
-    } else if(p.tht) {
-	return `
-	${standard}
-	${tht}
-	)
-	`
-    } 
-  }
 }
