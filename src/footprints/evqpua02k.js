b57c3159398fb833bf0fe93e3e3cb2f877292293
Sam Boysel <sboysel@gmail.com>
2023-07-26 12:22:09 -0700

    add smd/tht param for reset switch

--- src/footprints/evqpua02k.js
+++ src/footprints/evqpua02k.js
@@ -1,7 +1,7 @@
 module.exports = {
     params: {
         designator: 'R', // for reset
-        side: 'F', // not used
+        type: 'smd', // 'smd' or 'thru_hole'
         from: undefined,
         to: undefined
     },
@@ -34,10 +34,11 @@ module.exports = {
         (fp_line (start 2.4000000000 1.2500000000) (end 2.4000000000 1.2250000000) (layer F.SilkS) (width 0.15))
 
         ${'' /* pins */}
-        (pad 1 smd rect (at 2.6250000000 -1.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers F.Mask F.Paste F.Cu) ${p.from.str})
-        (pad 3 smd rect (at -2.6250000000 -1.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers F.Mask F.Paste F.Cu) ${p.to.str})
-        (pad 2 smd rect (at 2.6250000000 0.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers F.Mask F.Paste F.Cu) ${p.from.str})
-        (pad 4 smd rect (at -2.6250000000 0.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers F.Mask F.Paste F.Cu) ${p.to.str})
+        (pad 1 ${p.type} rect (at 2.6250000000 -1.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers *.Mask *.Cu) ${p.from.str})
+        (pad 3 ${p.type} rect (at -2.6250000000 -1.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers *.Mask *.Cu) ${p.to.str})
+        (pad 2 ${p.type} rect (at 2.6250000000 0.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers *.Mask *.Cu) ${p.from.str})
+        (pad 4 ${p.type} rect (at -2.6250000000 0.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers *.Mask *.Cu) ${p.to.str})
+
     )
 
     `
