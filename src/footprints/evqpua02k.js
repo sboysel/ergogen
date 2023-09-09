cd585d296fcf36eba140c32bb675b90cff93d442
Sam Boysel <sboysel@gmail.com>
2023-09-08 22:41:25 -0400

    evqpua02k: make silkscreen an optional param

--- src/footprints/evqpua02k.js
+++ src/footprints/evqpua02k.js
@@ -2,15 +2,12 @@ module.exports = {
     params: {
         designator: 'R', // for reset
         side: 'F',
+        silkscreen: true,
         from: undefined,
         to: undefined
     },
-    body: p => `
-    
-    (module Panasonic_Electronic_Components-EVQ-PUA02K-MFG (layer F.Cu) (tedit 5F8C6198)
-
-        ${p.at /* parametric position */}
-        
+    body: p => {
+      const silkscreen = `
         ${'' /* outline */}
         (fp_line (start -2.4000000000 2.3500000000) (end -2.4000000000 -2.3500000000) (layer ${p.side}.Fab) (width 0.15))
         (fp_line (start -2.4000000000 -2.3500000000) (end 2.4000000000 -2.3500000000) (layer ${p.side}.Fab) (width 0.15))
@@ -32,6 +29,15 @@ module.exports = {
         (fp_line (start -1.3000000000 2.3500000000) (end 1.3000000000 2.3500000000) (layer ${p.side}.SilkS) (width 0.15))
         (fp_line (start -2.4000000000 1.2500000000) (end -2.4000000000 1.2250000000) (layer ${p.side}.SilkS) (width 0.15))
         (fp_line (start 2.4000000000 1.2500000000) (end 2.4000000000 1.2250000000) (layer ${p.side}.SilkS) (width 0.15))
+      `
+    }
+
+    return `
+    (module Panasonic_Electronic_Components-EVQ-PUA02K-MFG (layer F.Cu) (tedit 5F8C6198)
+
+        ${p.at /* parametric position */}
+       
+        ${p.silkscreen ? silkscreen : ''} 
 
         ${'' /* pins */}
         (pad 1 smd rect (at 2.6250000000 -1.3500000000 ${p.rot})(size 1.5500000000 1.0000000000)(layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.from.str})
