f16b814e6673e2d82f935693354290099863925c
Sam Boysel <sboysel@gmail.com>
2023-09-16 17:19:36 -0400

    jstph: symmetric footprint option

--- src/footprints/jstph.js
+++ src/footprints/jstph.js
@@ -4,19 +4,22 @@
 // reverisble
 module.exports = {
     params: {
+        symmetric: true,
         pos: undefined,
         neg: undefined
     },
-    body: p => `
-    
-    (module JST_PH_S2B-PH-K_02x2.00mm_Angled (layer F.Cu) (tedit 58D3FE32)
-
-        ${p.at /* parametric position */}
-        (pad 1 thru_hole oval (at -2 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) ${p.neg.str})
-        (pad 2 thru_hole rect (at 0 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) ${p.pos.str})
+    body: p => {
+      const extra_neg = `
         (pad 3 thru_hole oval (at 2 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) ${p.neg.str})
-            
-    )
-    
-    `
+      `
+      return `
+      (module JST_PH_S2B-PH-K_02x2.00mm_Angled (layer F.Cu) (tedit 58D3FE32)
+
+          ${p.at /* parametric position */}
+          (pad 1 thru_hole oval (at -2 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) ${p.neg.str})
+          (pad 2 thru_hole rect (at 0 0 ${p.rot}) (size 1.2 1.7) (drill 0.75) (layers *.Cu *.Mask) ${p.pos.str})
+          ${p.symmatric ? extra_neg : ''} 
+      )
+      `
+    }
 }
