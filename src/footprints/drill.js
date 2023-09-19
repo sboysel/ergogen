65a2b780aeb0092b20dfb6667cb7720a15901c7c
Sam Boysel <sboysel@gmail.com>
2023-09-19 10:27:26 -0400

    fix drill params

--- src/footprints/drill.js
+++ src/footprints/drill.js
@@ -3,11 +3,12 @@ module.exports = {
     size: 2.2,
   },
   body: p => {
-    return `(module DrillHole (layer "F.Cu") (tedit 5F880A3E)
-    ${p.at}
-  (pad "" np_thru_hole circle (at 0 0) (size ${p.param.size} ${p.param.size}) (drill ${p.param.size}) (layers *.Cu *.Mask) )
-)
-`
+    return `
+      (module DrillHole (layer "F.Cu")
+      ${p.at}
+      (pad "" np_thru_hole circle (at 0 0) (size ${p.size} ${p.size}) (drill ${p.size}) (layers *.Cu *.Mask))
+    )
+    `
   }
 
 }
