179f397e00074debcef90d480348c4e8bb2c38a0
Sam Boysel <sboysel@gmail.com>
2023-10-19 20:23:22 -0400

    reversible solder nut

--- src/footprints/smtso2020mtj.js
+++ src/footprints/smtso2020mtj.js
@@ -10,7 +10,9 @@ module.exports = {
     (module "SMTSO2020MTJ" 
       ${p.at /* parametric position */}
 
-      (pad "" np_thru_hole circle (at 0 0) (size 3.8 3.8) (drill 3.8) (layers *.Mask) (tstamp 44216b30-6afe-425e-a43d-9a7ecfd6817d))
-      (pad "1" smd circle (at 0 0) (size 6.2 6.2) (layers "B.Cu" "B.Mask") (tstamp a69e605a-9052-440f-bcc9-68da52cfa154))
+      (pad "" np_thru_hole circle (at 0 0) (size 3.8 3.8) (drill 3.8) (layers *.Mask))
+      (pad "1" smd circle (at 0 0) (size 6.2 6.2) (layers "F.Cu" "F.Mask"))
+      (pad "1" smd circle (at 0 0) (size 6.2 6.2) (layers "B.Cu" "B.Mask"))
+
     )`
 }
