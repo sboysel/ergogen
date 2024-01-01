a770a8fdb8d489f8741ecc9914a036195ff222d3
Sam Boysel <sboysel@gmail.com>
2023-12-31 19:03:02 -0800

    testing

--- src/footprints/sod123.js
+++ src/footprints/sod123.js
@@ -11,10 +11,10 @@ module.exports = {
     (module D_SOD123 (layer ${ p.side }.Cu) (tedit 561B69D3)
       (attr smd)
       ${ p.at }
-      (fp_text reference D_SOD123 (at 0 1.925) (layer ${ p.side }.SilkS)
+      (fp_text reference D_SOD123 (at 0 1.925) (layer ${ p.side }.Fab)
         (effects (font (size 0.8 0.8) (thickness 0.15)))
       )
-      (fp_text value VAL** (at 0 -1.925) (layer ${ p.side }.SilkS) hide
+      (fp_text value VAL** (at 0 -1.925) (layer ${ p.side }.Fab) hide
         (effects (font (size 0.8 0.8) (thickness 0.15)))
       )
       (fp_line (start -3.075 1.2) (end -3.075 -1.2) (layer ${ p.side }.SilkS) (width 0.2))
