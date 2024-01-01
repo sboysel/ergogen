7ad61b8947e3bc19985dab4f44e6f12d7ec6c346
Sam Boysel <sboysel@gmail.com>
2023-12-31 16:13:20 -0800

    adding USB C

--- src/footprints/index.js
+++ src/footprints/index.js
@@ -39,6 +39,7 @@ module.exports = {
     standoff: require('./standoff'),
     text: require('./text'),
     trrs: require('./trrs'),
+    usbc: require('./usbc'),
     via: require('./via'),
     weact_pi_pico: require('./weact_pi_pico'),
     xiao: require('./xiao'),
