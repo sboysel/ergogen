0e4f0c16798a0d86003b0c11d2a1f9e49756ffc4
Sam Boysel <sboysel@gmail.com>
2024-01-06 21:15:34 -0500

    parameterize side for resistor + capacitor

--- src/footprints/capacitor.js
+++ src/footprints/capacitor.js
@@ -2,6 +2,7 @@ module.exports = {
   params: {
     designator: 'C',
     value: 'C0603',
+    side: 'B',
     from: { type: 'net', value: 'from' },
     to: { type: 'net', value: 'to' }
   },
@@ -12,24 +13,24 @@ module.exports = {
       (tags capacitor)
       (attr smd)
       ${ p.at }
-      (fp_text reference ${ p.ref } (at 0 -1.43) (layer F.Fab)
+      (fp_text reference ${ p.ref } (at 0 -1.43) (layer ${ p.side }.Fab)
         (effects (font (size 1 1) (thickness 0.15)))
       )
-      (fp_text value ${ p.value } (at 0 1.43) (layer F.Fab)
+      (fp_text value ${ p.value } (at 0 1.43) (layer ${ p.side }.Fab)
         (effects (font (size 1 1) (thickness 0.15)))
       )
-      (fp_line (start -0.8 0.4125) (end -0.8 -0.4125) (layer F.Fab) (width 0.1))
-      (fp_line (start -0.8 -0.4125) (end 0.8 -0.4125) (layer F.Fab) (width 0.1))
-      (fp_line (start 0.8 -0.4125) (end 0.8 0.4125) (layer F.Fab) (width 0.1))
-      (fp_line (start 0.8 0.4125) (end -0.8 0.4125) (layer F.Fab) (width 0.1))
-      (fp_line (start -0.237258 -0.5225) (end 0.237258 -0.5225) (layer F.SilkS) (width 0.12))
-      (fp_line (start -0.237258 0.5225) (end 0.237258 0.5225) (layer F.SilkS) (width 0.12))
-      (fp_line (start -1.48 0.73) (end -1.48 -0.73) (layer F.CrtYd) (width 0.05))
-      (fp_line (start -1.48 -0.73) (end 1.48 -0.73) (layer F.CrtYd) (width 0.05))
-      (fp_line (start 1.48 -0.73) (end 1.48 0.73) (layer F.CrtYd) (width 0.05))
-      (fp_line (start 1.48 0.73) (end -1.48 0.73) (layer F.CrtYd) (width 0.05))
-      (pad 1 smd roundrect (at -0.825 0) (size 0.8 0.95) (layers F.Cu F.Mask F.Paste) (roundrect_rratio 0.25) ${ p.from.str })
-      (pad 2 smd roundrect (at 0.825 0) (size 0.8 0.95) (layers F.Cu F.Mask F.Paste) (roundrect_rratio 0.25) ${ p.to.str })
+      (fp_line (start -0.8 0.4125) (end -0.8 -0.4125) (layer ${ p.side }.Fab) (width 0.1))
+      (fp_line (start -0.8 -0.4125) (end 0.8 -0.4125) (layer ${ p.side }.Fab) (width 0.1))
+      (fp_line (start 0.8 -0.4125) (end 0.8 0.4125) (layer ${ p.side }.Fab) (width 0.1))
+      (fp_line (start 0.8 0.4125) (end -0.8 0.4125) (layer ${ p.side }.Fab) (width 0.1))
+      (fp_line (start -0.237258 -0.5225) (end 0.237258 -0.5225) (layer ${ p.side }.SilkS) (width 0.12))
+      (fp_line (start -0.237258 0.5225) (end 0.237258 0.5225) (layer ${ p.side }.SilkS) (width 0.12))
+      (fp_line (start -1.48 0.73) (end -1.48 -0.73) (layer ${ p.side }.CrtYd) (width 0.05))
+      (fp_line (start -1.48 -0.73) (end 1.48 -0.73) (layer ${ p.side }.CrtYd) (width 0.05))
+      (fp_line (start 1.48 -0.73) (end 1.48 0.73) (layer ${ p.side }.CrtYd) (width 0.05))
+      (fp_line (start 1.48 0.73) (end -1.48 0.73) (layer ${ p.side }.CrtYd) (width 0.05))
+      (pad 1 smd roundrect (at -0.825 0) (size 0.8 0.95) (layers ${ p.side }.Cu ${ p.side }.Mask ${ p.side }.Paste) (roundrect_rratio 0.25) ${ p.from.str })
+      (pad 2 smd roundrect (at 0.825 0) (size 0.8 0.95) (layers ${ p.side }.Cu ${ p.side }.Mask ${ p.side }.Paste) (roundrect_rratio 0.25) ${ p.to.str })
     )
     `
   }
